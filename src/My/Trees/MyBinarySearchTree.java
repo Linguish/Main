2c58dfd171e77d1d23990a81d0e49171bf08c820
cvuuhk <36870468+cvuuhk@users.noreply.github.com>
2019-04-26 09:49:54 +0800

    将节点放入内部类

--- src/My/Trees/MyBinarySearchTree.java
+++ src/My/Trees/MyBinarySearchTree.java
@@ -1,9 +1,29 @@
-package My;
-
-import My.Trees.TreeNode;
+package My.Trees;
 
 public class MyBinarySearchTree<E>
 {
+    private class TreeNode<E>
+    {
+        private E data;
+        private TreeNode<E> leftChild = null;
+        private TreeNode<E> rightChild = null;
+        public TreeNode(){}
+        public TreeNode(E data){this.data = data;}
+
+        public E getData(){ return data; }
+        public void setData(E data){ this.data = data; }
+        public TreeNode<E> getLeftChild(){ return leftChild; }
+        public void setLeftChild(TreeNode<E> leftChild){ this.leftChild = leftChild; }
+        public TreeNode<E> getRightChild(){ return rightChild; }
+        public void setRightChild(TreeNode<E> rightChild){ this.rightChild = rightChild; }
+        public void setNode(TreeNode<E> node)
+        {
+            this.data = node.data;
+            this.leftChild = node.leftChild;
+            this.rightChild = node.rightChild;
+        }
+    }
+    
     private TreeNode<E> root = null;
     
     public MyBinarySearchTree(){}
